7b24d208f047538e11aaaa88e17d11d4094e6696
yellowgh0st <burim.berisa@protonmail.com>
2020-06-22 02:33:16 +0200

    No Data Exception

--- README.md
+++
@@ -1,2 +0,0 @@
-# vether-discord-bot
-Price bot
--- app.js
+++ app.js
@@ -19,18 +19,23 @@ async function getSummaryData(url = summary) {
 async function sendPriceToChannel(message) {
   try {
     const data = await getSummaryData();
+    let announceMessage;
     
     const usdcPool = data.ETH_0x97deC872013f6B5fB443861090ad931542878126;
     const vetherPool = data.ETH_0x506D07722744E4A390CD7506a2Ba1A8157E63745;
     const daiPool = data.ETH_0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667;
     
-    const eth = (1/Number(vetherPool.last_price)).toFixed(5);
-    const usdc = (eth*Number(usdcPool.last_price)).toFixed(2);
-    const dai = (eth*Number(daiPool.last_price)).toFixed(2);
-    
-    const announceMessage = `
-    <:uniswap:718587420274196553> Uniswap V1 **$VETH** price is at *USDC* **${usdc}**, *DAI* **${dai}**, *Ξ* **${eth}**
-    `
+    if(vetherPool !== undefined) {
+        const eth = (1/Number(vetherPool.last_price)).toFixed(5);
+        const usdc = (eth*Number(usdcPool.last_price)).toFixed(2);
+        const dai = (eth*Number(daiPool.last_price)).toFixed(2);
+        
+        announceMessage = `
+        <:uniswap:718587420274196553> Uniswap V1 **$VETH** price is at *USDC* **${usdc}**, *DAI* **${dai}**, *Ξ* **${eth}**
+        `
+    } else {
+        announceMessage = `<:joint:716869960496054273> No data`
+    }
     
     message.channel.send(announceMessage);
   }
