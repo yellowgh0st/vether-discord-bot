60e43cecaeab349f45c9e8eebdafc3b70e490843
yellowgh0st <burim.berisa@protonmail.com>
2020-06-26 02:30:09 +0200

    Make it work with Uniswap V2

--- app.js
+++ app.js
@@ -1,51 +1,76 @@
-const fetch = require('node-fetch');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const key = process.env.BOT_LOGIN_KEY
+const Discord = require('discord.js')
+const Web3 = require('web3')
+const UniswapPair = require('./abi/UniswapPair.json')
 
-const summary = 'https://api.uniswap.info/v1/summary';
+const client = new Discord.Client()
+const botLoginKey = process.env.BOT_LOGIN_KEY
 
-async function getSummaryData(url = summary) {
+const infuraAPI = 'https://mainnet.infura.io/v3/'
+    + process.env.INFURA_API_KEY
+
+const poolVethEth = '0x03e008804c5bf70e20b5a0b7233cf2687ccd2a96'
+const poolDaiEth = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11'
+const poolUsdcEth = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
+
+async function getPrice(pair, swap) {
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+      const web3 = new Web3(new Web3.providers.HttpProvider(infuraAPI))
+      const contract = new web3.eth.Contract(UniswapPair.abi, pair)
+      const data = await contract.methods.getReserves().call()
+      if(swap) {
+          return (data.reserve1/data.reserve0)
+      } else {
+          return (data.reserve0/data.reserve1)
+      }
   }
   catch (e) {
-    console.log(e);
+      console.log(e);
   }
 }
 
 async function sendPriceToChannel(message) {
   try {
-    const data = await getSummaryData();
-    let announceMessage;
-    
-    const usdcPool = data.ETH_0x97deC872013f6B5fB443861090ad931542878126;
-    const vetherPool = data.ETH_0x506D07722744E4A390CD7506a2Ba1A8157E63745;
-    const daiPool = data.ETH_0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667;
-    
-    if(vetherPool !== undefined) {
-        const eth = (1/Number(vetherPool.last_price)).toFixed(5);
-        const usdc = (eth*Number(usdcPool.last_price)).toFixed(2);
-        const dai = (eth*Number(daiPool.last_price)).toFixed(2);
-        
-        announceMessage = `
-        <:uniswap:718587420274196553> Uniswap V1 **$VETH** price is at *USDC* **${usdc}**, *DAI* **${dai}**, *Ξ* **${eth}**
-        `
-    } else {
-        announceMessage = `<:joint:716869960496054273> No data`
-    }
-    
-    message.channel.send(announceMessage);
+      const regExp = /e-(\d+)/;
+
+      let priceVethEth = await getPrice(poolVethEth, true)
+          priceVethEth = (priceVethEth) ? priceVethEth.toFixed(6) : null
+      console.log(priceVethEth)
+
+      let priceEthDai = await getPrice(poolDaiEth)
+          priceEthDai = (priceEthDai) ? priceEthDai.toString() : null
+      console.log(priceEthDai)
+
+      let priceEthUsdc = await getPrice(poolUsdcEth)
+          priceEthUsdc = (priceEthUsdc) ? priceEthUsdc.toString() : null
+      console.log(priceEthUsdc)
+
+      let priceVethDai = priceVethEth * priceEthDai
+          priceVethDai = priceVethDai.toString()
+          priceVethDai = priceVethDai.replace(regExp, '')
+          priceVethDai = Number(priceVethDai).toFixed(2)
+      console.log(priceVethDai)
+
+      let priceVethUsdc = priceVethEth * priceEthUsdc
+          priceVethUsdc = priceVethUsdc.toString()
+          priceVethUsdc = priceVethUsdc.replace(regExp, '')
+          priceVethUsdc = Number(priceVethUsdc).toFixed(2)
+      console.log(priceVethUsdc)
+
+      announceMessage = `
+         <:uniswap:718587420274196553> Uniswap V2 **$VETH** price is at *USDC* **${priceVethUsdc}**, *DAI* **${priceVethDai}**, *Ξ* **${priceVethEth}**
+       `
+
+      //   announceMessage = `<:joint:716869960496054273> No data`
+
+      await message.channel.send(announceMessage);
   }
   catch (e) {
-    console.log(e);
+      console.log(e);
   }
 }
 
 client.once('ready', () => {
-    console.log('Hey, I\'m the new Price Bot and I\'m Ready!');
+    console.log('Hey, I\'m the new Price Bot and I\'m Ready!')
 });
 
 client.on('message', message => {
@@ -56,4 +81,4 @@ client.on('message', message => {
     }
 });
 
-client.login(key);
+client.login(botLoginKey);
