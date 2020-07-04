a401c152f5f66aef88013c8c1365e7aabc913675
yellowgh0st <burim.berisa@protonmail.com>
2020-07-04 04:27:25 +0200

    Add Resfinex exchange

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const Web3 = require('web3')
 const UniswapPair = require('./abi/UniswapPair.json')
+const fetch = require('node-fetch')
 
 const client = new Discord.Client()
 const botLoginKey = process.env.BOT_LOGIN_KEY
@@ -8,87 +9,120 @@ const botLoginKey = process.env.BOT_LOGIN_KEY
 const infuraAPI = 'https://mainnet.infura.io/v3/'
     + process.env.INFURA_API_KEY
 
-const poolVethEth = '0x3696fa5ad6e5c74fdcbced9af74379d94c4b775a'
-const poolDaiEth = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11'
-const poolUsdcEth = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
-
-async function getPrice(pair, swap) {
-  try {
-      const web3 = new Web3(new Web3.providers.HttpProvider(infuraAPI))
-      const contract = new web3.eth.Contract(UniswapPair.abi, pair)
-      const data = await contract.methods.getReserves().call()
-      if(swap) {
-          return (data.reserve1/data.reserve0)
-      } else {
-          return (data.reserve0/data.reserve1)
-      }
-  }
-  catch (e) {
-      console.log(e)
-  }
+async function uniswapPrice(pair, swap) {
+	try {
+		const web3 = new Web3(new Web3.providers.HttpProvider(infuraAPI))
+		const contract = new web3.eth.Contract(UniswapPair.abi, pair)
+		const data = await contract.methods.getReserves().call()
+		if(swap) {
+			return (data.reserve1 / data.reserve0)
+		}
+		else {
+			return (data.reserve0 / data.reserve1)
+		}
+	}
+	catch (e) {
+		console.log(e)
+	}
 }
 
-async function sendPriceToChannel(message) {
-  try {
-      const regExp = /e-(\d+)/;
+async function resfinexPrice(market) {
+	try {
+		const response = await fetch('https://api.resfinex.com/engine/history?pair=' + market)
+		const resfinex = await response.json()
+		const last = resfinex.data.pop()
+		return last.price
+	}
+	catch (e) {
+		console.log(e)
+	}
+}
 
-      let priceVethEth = await getPrice(poolVethEth, true)
-          priceVethEth = (priceVethEth) ? priceVethEth.toFixed(6) : null
-      console.log(priceVethEth)
+async function sendPriceToChannel(message, exchange) {
+	try {
+		const regExp = /e-(\d+)/
+		let announceMessage
 
-      let priceEthDai = await getPrice(poolDaiEth)
-          priceEthDai = (priceEthDai) ? priceEthDai.toString() : null
-      console.log(priceEthDai)
+		let uniswapVethEth = await uniswapPrice('0x3696fa5ad6e5c74fdcbced9af74379d94c4b775a', true)
+		uniswapVethEth = (uniswapVethEth) ? uniswapVethEth.toFixed(6) : '<:joint:716869960496054273> No data'
+		console.log(uniswapVethEth)
 
-      let priceEthUsdc = await getPrice(poolUsdcEth)
-          priceEthUsdc = (priceEthUsdc) ? priceEthUsdc.toString() : null
-      console.log(priceEthUsdc)
+		let uniswapEthDai = await uniswapPrice('0xa478c2975ab1ea89e8196811f51a7b7ade33eb11')
+		uniswapEthDai = (uniswapEthDai) ? uniswapEthDai.toFixed(2) : null
+		console.log(uniswapEthDai)
 
-      let priceVethDai = priceVethEth * priceEthDai
-          priceVethDai = priceVethDai.toString()
-          priceVethDai = priceVethDai.replace(regExp, '')
-          priceVethDai = Number(priceVethDai).toFixed(2)
-      console.log(priceVethDai)
+		let uniswapEthUsdc = await uniswapPrice('0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc')
+		uniswapEthUsdc = (uniswapEthUsdc) ? (uniswapEthUsdc * 1000000000000).toFixed(2) : null
+		console.log(uniswapEthUsdc)
 
-      let priceVethUsdc = priceVethEth * priceEthUsdc
-          priceVethUsdc = priceVethUsdc.toString()
-          priceVethUsdc = priceVethUsdc.replace(regExp, '')
-          priceVethUsdc = Number(priceVethUsdc).toFixed(2)
-      console.log(priceVethUsdc)
+		let uniswapVethDai = uniswapVethEth * uniswapEthDai
+		uniswapVethDai = uniswapVethDai.toString()
+		uniswapVethDai = uniswapVethDai.replace(regExp, '')
+		uniswapVethDai = Number(uniswapVethDai).toFixed(2)
+		uniswapVethDai = (uniswapVethDai === null) ? '<:joint:716869960496054273> No data' : uniswapVethDai
+		console.log(uniswapVethDai)
 
-      // announceMessage = `
-      //    <:uniswap:718587420274196553> Uniswap V2 **$VETH** price is at *USDC* **${priceVethUsdc}**, *DAI* **${priceVethDai}**, *Ξ* **${priceVethEth}**
-      //  `
+		let uniswapVethUsdc = uniswapVethEth * uniswapEthUsdc
+		uniswapVethUsdc = uniswapVethUsdc.toString()
+		uniswapVethUsdc = uniswapVethUsdc.replace(regExp, '')
+		uniswapVethUsdc = Number(uniswapVethUsdc).toFixed(2)
+		uniswapVethUsdc = (uniswapVethUsdc === null) ? '<:joint:716869960496054273> No data' : uniswapVethUsdc
+		console.log(uniswapVethUsdc)
 
-      announceMessage = `
-         <:uniswap:718587420274196553> Uniswap V2 **$VETH** price is at *DAI* **${priceVethDai}**, *Ξ* **${priceVethEth}**
-       `
+		let resfinexVethEth = await resfinexPrice('VETH_ETH')
+		resfinexVethEth = (resfinexVethEth) ? resfinexVethEth.toFixed(5) : '<:joint:716869960496054273> No data'
+		console.log(resfinexVethEth)
 
-      //   announceMessage = `<:joint:716869960496054273> No data`
+		let resfinexEthUsdt = await resfinexPrice('ETH_USDT')
+		resfinexEthUsdt = (resfinexEthUsdt) ? resfinexEthUsdt.toFixed(2) : '<:joint:716869960496054273> No data'
+		console.log(resfinexEthUsdt)
 
-      await message.channel.send(announceMessage)
-  }
-  catch (e) {
-      console.log(e)
-  }
+		let resfinexVethUsdt = resfinexVethEth * resfinexEthUsdt
+		resfinexVethUsdt = (resfinexVethUsdt) ? resfinexVethUsdt.toFixed(2) : '<:joint:716869960496054273> No data'
+		console.log(resfinexVethUsdt)
+
+		if(exchange === 'uniswap') {
+			announceMessage = `<:uniswap:718587420274196553> Uniswap V2 **$VETH** price is at *USDC* **${uniswapVethUsdc}**, *DAI* **${uniswapVethDai}**, *Ξ* **${uniswapVethEth}**`
+		}
+		else if(exchange === 'resfinex') {
+			announceMessage = `<:resfinex:728785990675857428> Resfinex **$VETH** price is at *USDT* **${resfinexVethUsdt}**, *Ξ* **${resfinexVethEth}**`
+		}
+		else {
+			announceMessage = `<:uniswap:718587420274196553> Uniswap V2 **$VETH** price is at *USDC* **${uniswapVethUsdc}**, *DAI* **${uniswapVethDai}**, *Ξ* **${uniswapVethEth}**
+<:resfinex:728785990675857428> Resfinex **$VETH** price is at *USDT* **${resfinexVethUsdt}**, *Ξ* **${resfinexVethEth}**`
+		}
+
+		if(announceMessage) {
+			await message.channel.send(announceMessage)
+		}
+	}
+	catch (e) {
+		console.log(e)
+	}
 }
 
 client.once('ready', () => {
-    console.log('Hey, I\'m the new Price Bot and I\'m Ready!')
-});
+	console.log('Hey, I\'m the new Price Bot and I\'m Ready!')
+})
 
 client.on('message', message => {
 
-    if (message.channel.name === 'trading') {
-        switch (message.content) {
-            case '.':
-                sendPriceToChannel(message)
-                break
-            case '!price':
-                sendPriceToChannel(message)
-        }
-    }
+	if (message.channel.name === 'trading') {
+		switch (message.content) {
+		case '.':
+			sendPriceToChannel(message)
+			break
+		case '!price':
+			sendPriceToChannel(message)
+			break
+		case '!uniswap':
+			sendPriceToChannel(message, 'uniswap')
+			break
+		case '!resfinex':
+			sendPriceToChannel(message, 'resfinex')
+		}
+	}
 
-});
+})
 
-client.login(botLoginKey);
+client.login(botLoginKey)
